996570c341830a1cd33bf157d0e009b70fd4aa83
Rick Copeland <rick@arborian.com>
2019-02-18 18:59:37 -0500

    Add test

---
+++ recommender/stage1/tests/test_train_test_split.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+
+import numpy as np
+import pandas as pd
+
+class TestTestTrainSplit(TestCase):
+
+    def setUp(self):
+        values = np.r_[:100]
+        self.df = pd.DataFrame({
+            'visitorid': values,
+            'anotherid': values[::-1],
+        })
+
+    def test_execute_defaults(self):
+        step = TrainTestSplit()
+        df_train, df_test = step.execute(self.df)
+        self.assertEqual(len(df_train), 90)
+        self.assertEqual(df_test.visitorid.min(), 90)
+
+    def test_execute_50(self):
+        step = TrainTestSplit(train_percentile=50)
+        df_train, df_test = step.execute(self.df)
+        self.assertEqual(len(df_train), 50)
+        self.assertEqual(df_test.visitorid.min(), 50)
+
+    def test_execute_othercol(self):
+        step = TrainTestSplit(column_name='anotherid')
+        df_train, df_test = step.execute(self.df)
+        self.assertEqual(len(df_train), 90)
+        self.assertEqual(df_test.visitorid.min(), 0)
+        self.assertEqual(df_test.anotherid.min(), 90)
